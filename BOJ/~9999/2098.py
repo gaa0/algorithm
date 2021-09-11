056af3ea2bf78557c4dba9b5341e1feff35d643b
gaa0 <lotte1071103@naver.com>
2021-09-11 23:14:54 +0900

    2021.09.11 Update BOJ

--- BOJ/~9999/2098.py
+++ BOJ/~9999/2098.py
@@ -3,28 +3,28 @@ import sys
 sys.stdin = open('input.txt')
 sys.setrecursionlimit(10**6)
 
-
-def tsp(N, costs):
-    dp = [[None] * (1 << N) for _ in range(N)]
-    INF = float('inf')
-
-    def find_path(last, visited):
-        if visited == (1 << N) - 1:
-            return costs[last][0] or INF
-
-        if dp[last][visited]:
-            return dp[last][visited]
-
-        tmp = INF
-        for city in range(N):
-            if visited & (1 << city) == 0 and costs[last][city] != 0:
-                tmp = min(tmp, find_path(city, visited | (1 << city)) + costs[last][city])
-        dp[last][visited] = tmp
-        return tmp
-
-    return find_path(0, 1 << 0)
-
-
 N = int(sys.stdin.readline())
-costs = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
-print(tsp(N, costs))
+W = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dp = [[0] * (1 << N) for _ in range(N)]
+
+
+def tsp(last, visited):
+    if visited == (1 << N) - 1:
+        if W[last][0]:
+            return W[last][0]
+        else:
+            return float('inf')
+
+    if dp[last][visited]:
+        return dp[last][visited]
+
+    min_cost = float('inf')
+    for i in range(N):
+        if visited & (1 << i) != 1 << i and W[last][i]:
+            min_cost = min(min_cost, tsp(i, visited | (1 << i)) + W[last][i])
+    dp[last][visited] = min_cost
+    return min_cost
+
+
+ans = tsp(0, 1 << 0)
+print(ans)
