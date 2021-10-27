190e061ccba0b661e9eab4760beb364177bfa50d
gaa0 <lotte1071103@naver.com>
2021-10-27 23:35:07 +0900

    2021.10.27 Update programmers

--- programmers/60061.py
+++ programmers/60061.py
@@ -57,8 +57,3 @@ def solution(n, build_frame):
                     bf_list.append([x, y, 1])
     bf_set = sorted(bf_list)
     return bf_set
-
-
-n = 5
-build_frame = 	[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
-print(solution(n, build_frame))
