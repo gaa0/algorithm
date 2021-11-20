b17e4cae87b29ebd05c02b5ace15aeec5288108e
gaa0 <lotte1071103@naver.com>
2021-11-20 23:52:51 +0900

    2021.11.20 Add BOJ

---
+++ BOJ/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // char str[1000000000];
+    // int sort[10] = { 0 };
+    // scanf_s("%s", str);
+    // for (int s = 0; s < 1000000000; s++) {
+    //     if (str[s]) {
+    //         int num = atoi(str[s]);
+    //         sort[num] += 1;
+    //     }
+    // }
+    // for (int i = 0; i < 10; i++) {
+    //     if (sort[i] > 0) {
+    //         for (int j = 0; j < sort[i]; j++) {
+    //             printf("%d", i);
+    //         }
+    //     }
+    // }
+    printf("check");
+}
