06fad8405be86fd70bfe6b2e22e73d15bac193c8
Gayoung Kim <lotte1071103@naver.com>
2025-12-05 20:23:34 +0000

    Time: 0 ms (100.00%) | Memory: 57.2 MB (87.17%) - LeetSync

--- LeetCode/792-binary-search/binary-search.js
+++ LeetCode/792-binary-search/binary-search.js
@@ -4,5 +4,23 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target);
+    let start = 0;
+    let end = nums.length;
+    while (start <= end) {
+        let idx = 0;
+        if ((end + start) % 2 === 0) {
+            idx = Math.floor((end + start) / 2);
+        } else {
+            idx = Math.floor((end + start) / 2);
+        }
+        const st = nums[idx];
+        if (target === st) {
+            return idx;
+        } else if (target < st) {
+            end = idx - 1;
+        } else {
+            start = idx + 1;
+        }
+    }
+    return -1;
 };
