7578654f768945f4acde2401a9500ddae2c7cc09
Gayoung Kim <lotte1071103@naver.com>
2025-12-06 13:37:51 +0000

    Time: 0 ms (100.00%) | Memory: 59.3 MB (25.76%) - LeetSync

--- LeetCode/733-flood-fill/flood-fill.js
+++ LeetCode/733-flood-fill/flood-fill.js
@@ -8,34 +8,23 @@
 var floodFill = function (image, sr, sc, color) {
     const dx = [-1, 1, 0, 0];
     const dy = [0, 0, -1, 1];
+    const st = image[sr][sc];
+    if (st === color) return image;
 
-    function dfs(x, y, visited, grid, st, color) {
-        const myStack = [[x, y]];
-        while (myStack.length) {
-            const [x, y] = myStack.pop();
-            visited[x][y] = true;
-            grid[x][y] = color;
+    const myStack = [[sr, sc]];
+    while (myStack.length) {
+        const [x, y] = myStack.pop();
+        image[x][y] = color;
 
-            for (let d = 0; d < 4; d++) {
-                const nx = x + dx[d];
-                const ny = y + dy[d];
-                if (0 <= nx && nx < grid.length && 0 <= ny && ny < grid[0].length &&
-                    !visited[nx][ny] && grid[nx][ny] === st) {
-                    myStack.push([nx, ny]);
-                }
+        for (let d = 0; d < 4; d++) {
+            const nx = x + dx[d];
+            const ny = y + dy[d];
+            if (0 <= nx && nx < image.length && 0 <= ny &&
+                ny < image[0].length && image[nx][ny] === st) {
+                myStack.push([nx, ny]);
             }
         }
-        return grid;
     }
 
-    const visited = [];
-    for (let i = 0; i < image.length; i++) {
-        const inner = [];
-        for (let j = 0; j < image[0].length; j++) {
-            inner.push(false);
-        }
-        visited.push(inner);
-    }
-
-    return dfs(sr, sc, visited, image, image[sr][sc], color);
+    return image;
 };
