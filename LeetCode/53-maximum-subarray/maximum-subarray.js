55c4a286b71e6dce2788359c3ee83de412aaed61
Gayoung Kim <lotte1071103@naver.com>
2025-12-06 14:33:02 +0000

    Time: 1 ms (84.70%) | Memory: 64.5 MB (45.64%) - LeetSync

--- LeetCode/53-maximum-subarray/maximum-subarray.js
+++ LeetCode/53-maximum-subarray/maximum-subarray.js
@@ -3,28 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    let positiveSum = 0;
-    let flag = 1;
-    for (const num of nums) {
-        if (num > 0) {
-            positiveSum += num;
-        } else {
-            flag = 0;
-        }
-    }
-    if (flag == 1) return positiveSum;
+    let current = nums[0];
+    let max = nums[0];
 
-    let maxSum = nums[0];
-
-    for (let i = 0; i < nums.length; i++) {
-        let currentSum = nums[i];
-        maxSum = Math.max(currentSum, maxSum);
-        for (let j = i + 1; j < nums.length; j++) {
-            currentSum += nums[j];
-            maxSum = Math.max(currentSum, maxSum);
-            if (currentSum < 0) break;
-        }
+    for (let i = 1; i < nums.length; i++) {
+        current = Math.max(nums[i], current + nums[i]);
+        max = Math.max(max, current);
     }
 
-    return maxSum;
+    return max;
 };
