01f36f09a2247f29e7bfdb66200625d2d38ae811
Gayoung Kim <lotte1071103@naver.com>
2025-12-30 12:31:29 +0000

    Time: 396 ms (60.32%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/619-biggest-single-number/biggest-single-number.sql
+++ LeetCode/619-biggest-single-number/biggest-single-number.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-(select num from MyNumbers group by num having count(*) = 1 order by num desc limit 1)
-union
-(select null as num from MyNumbers) limit 1;
+select
+    coalesce((select num from MyNumbers group by num having count(*) = 1 order by num desc limit 1), null) as num;
