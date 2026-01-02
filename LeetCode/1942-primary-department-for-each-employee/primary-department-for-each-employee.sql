bec5cb021cdbb671e97c5119ba4b5a2e81d514bc
Gayoung Kim <lotte1071103@naver.com>
2026-01-02 22:09:58 +0000

    Time: 481 ms (87.04%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ LeetCode/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 select a.employee_id, ifnull(b.department_id, a.department_id) as department_id
 from Employee a
-left join (select * from Employee where primary_flag = 'Y') as b
+left join Employee b
 on a.employee_id = b.employee_id
+and b.primary_flag = 'Y'
 group by a.employee_id;
