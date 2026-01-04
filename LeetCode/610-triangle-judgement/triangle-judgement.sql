0282910bfbf9caf27420a3bac2790c5320a49e83
Gayoung Kim <lotte1071103@naver.com>
2026-01-04 00:36:23 +0000

    Time: 271 ms (58.14%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/610-triangle-judgement/triangle-judgement.sql
+++ LeetCode/610-triangle-judgement/triangle-judgement.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
 select
     *,
-    (case when (x > y and x > z and x >= y + z)
-    or (y > x and y > z and y >= x + z)
-    or (z > x and z > y and z >= x + y) then 'No'
-    else 'Yes' end)
-    as triangle
+    (case when x < y + z and y < x + z and z < x + y then 'Yes' else 'No' end) as triangle
 from Triangle;
