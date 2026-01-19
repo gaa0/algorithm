cb02efe03b2fd3f8feebcd8f8f0f12b88a476784
Gayoung Kim <lotte1071103@naver.com>
2026-01-19 23:06:40 +0000

    Time: 356 ms (10.08%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ LeetCode/602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -6,7 +6,3 @@ select distinct a.requester_id as id, ifnull(b.num, 0) + ifnull(c.num, 0) as num
     ) a
 left join (select requester_id, count(accepter_id) as num from RequestAccepted group by requester_id) as b on a.requester_id = b.requester_id
 left join (select accepter_id, count(requester_id) as num from RequestAccepted group by accepter_id) as c on a.requester_id = c.accepter_id order by num desc limit 1;
-
--- select * from RequestAccepted a
--- left join (select requester_id, count(accepter_id) as num from RequestAccepted group by requester_id) as b on a.requester_id = b.requester_id
--- left join (select accepter_id, count(requester_id) as num from RequestAccepted group by accepter_id) as c on a.requester_id = c.accepter_id;
