5927a2723a5510398d13a42ef3e27d0e1b28b5f3
Gayoung Kim <lotte1071103@naver.com>
2026-01-21 22:54:38 +0000

    Time: 372 ms (22.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select visited_on, amount, average_amount from (
+select visited_on, sum(amount) over(order by visited_on range between interval 6 day preceding and current row) as amount, round(avg(amount) over(order by visited_on range between interval 6 day preceding and current row), 2) as average_amount, count(amount) over(order by visited_on range between interval 6 day preceding and current row) as cnt from (select visited_on, sum(amount) as amount from Customer group by visited_on) b) a where cnt >= 7;
