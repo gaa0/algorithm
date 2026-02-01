2d945a3b05e85b8dabff9aa71aa3ca5d8e1766c2
Gayoung Kim <lotte1071103@naver.com>
2026-02-01 21:36:55 +0000

    Time: 32 ms (97.06%) | Memory: 52.8 MB (93.70%) - LeetSync

---
+++ LeetCode/2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let flag = 0;
+    return function(...args){
+        if (!flag) {
+            flag = 1;
+            return fn(...args);
+        } else {
+            return;
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
