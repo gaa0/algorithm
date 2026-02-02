1ac0905fd23d04ad6d9b50d4dc40a94fd029e85f
Gayoung Kim <lotte1071103@naver.com>
2026-02-02 22:47:38 +0000

    Time: 268 ms (46.24%) | Memory: 96.5 MB (22.73%) - LeetSync

---
+++ LeetCode/2731-memoize/memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const m = new Map();
+    return function(...args) {
+        const s = args.toString();
+        if (m.has(s)) {
+            console.log(1);
+            return m.get(s);
+        } else {
+            m.set(s, fn(...args));
+            return m.get(s);
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
