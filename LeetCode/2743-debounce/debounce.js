409102c54950b4de807fd8b15a2bc2bbc741e764
Gayoung Kim <lotte1071103@naver.com>
2026-02-17 22:44:03 +0000

    Time: 55 ms (27.86%) | Memory: 53.7 MB (81.82%) - LeetSync

--- LeetCode/2743-debounce/debounce.js
+++ LeetCode/2743-debounce/debounce.js
@@ -4,16 +4,10 @@
  * @return {Function}
  */
 var debounce = function(fn, t) {
-    let flag = 0;
-    let s;
+    let timer = null;
     return function(...args) {
-        if (flag) {
-            clearTimeout(s);
-            clearTimeout(s2);
-        }
-        s = setTimeout(() => fn(...args), t);
-        flag = 1;
-        s2 = setTimeout(() => {flag = 0;}, t);
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), t);
     }
 };
 
