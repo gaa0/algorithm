19edbfb144825906e9b35fa7eb110f2f5d1cd483
Gayoung Kim <lotte1071103@naver.com>
2026-02-24 22:02:50 +0000

    Time: 114 ms (29.94%) | Memory: 75 MB (92.32%) - LeetSync

---
+++ LeetCode/2742-group-by/group-by.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const grouped = new Object;
+    for (const item of this) {
+        const o = fn(item);
+        if (o in grouped) {
+            grouped[o].push(item);
+        } else {
+            grouped[o] = [item];
+        }
+    }
+    return grouped;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
