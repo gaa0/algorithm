8ca375bb21d4c1ddef915ab6ec2c5219f0376bbd
Gayoung Kim <lotte1071103@naver.com>
2026-03-10 22:27:57 +0000

    Time: 2 ms (76.43%) | Memory: 57.8 MB (60.41%) - LeetSync

--- LeetCode/459-repeated-substring-pattern/repeated-substring-pattern.js
+++ LeetCode/459-repeated-substring-pattern/repeated-substring-pattern.js
@@ -3,15 +3,5 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function(s) {
-    const sl = s.length;
-    for (let i = 1; i <= Math.floor(sl / 2) ; i++) {
-        if (s.length % i !== 0) continue;
-        const sub = s.slice(0, i);
-        let cnt = 0;
-        for (let j = 0; j < sl; j += i) {
-            if (s.slice(j, j + i) === sub) cnt += 1;
-        }
-        if (cnt === sl / i) return true;
-    }
-    return false;
+    return (s + s).indexOf(s, 1) !== s.length;
 };
