b46f79a1f400bd548612bc0061ef68bba8e8e26b
Gayoung Kim <lotte1071103@naver.com>
2026-03-17 23:24:19 +0000

    Time: 0 ms (100.00%) | Memory: 54 MB (35.88%) - LeetSync

--- LeetCode/58-length-of-last-word/length-of-last-word.js
+++ LeetCode/58-length-of-last-word/length-of-last-word.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    return s.trim().split(' ').pop().length;
+    let len = 0;
+    let i = s.length - 1;
+
+    while (s[i] === ' ') i--;
+    while (i >= 0 && s[i] !== ' ') {
+        len++;
+        i--;
+    };
+
+    return len;
 };
