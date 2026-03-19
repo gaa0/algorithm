dce41f2e2838b920492b051b406a6afa16ae6f64
Gayoung Kim <lotte1071103@naver.com>
2026-03-19 23:04:10 +0000

    Time: 1 ms (77.67%) | Memory: 55.3 MB (26.49%) - LeetSync

---
+++ LeetCode/682-baseball-game/baseball-game.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    const records = [];
+    for (const operation of operations) {
+        if (Number(operation) >= 0 || Number(operation) <= 9) {
+            records.push(Number(operation));
+        } else if (operation === '+') {
+            const rl = records.length;
+            records.push(records[rl - 2] + records[rl - 1]);
+        } else if (operation === 'D') {
+            records.push(records[records.length - 1] * 2);
+        } else if (operation === 'C') {
+            records.pop();
+        }
+    }
+    return records.reduce((a, b) => a + b, 0);
+};
