d84ffda6c7bf37aca9b5c311efb3bc3d11171511
Gayoung Kim <lotte1071103@naver.com>
2026-03-22 23:41:21 +0000

    Time: 0 ms (100.00%) | Memory: 53 MB (95.24%) - LeetSync

--- LeetCode/1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.js
+++ LeetCode/1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.js
@@ -3,32 +3,17 @@
  * @return {string}
  */
 var tictactoe = function (moves) {
-    const grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-    let i = 0;
-    for (const move of moves) {
-        let turn;
-        if (i % 2 === 0) turn = 'A';
-        else turn = 'B';
-        i++;
-        const x = move[0];
-        const y = move[1];
-        grid[x][y] = turn;
+    const grid = [['', '', ''], ['', '', ''], ['', '', '']];
+    for (let i = 0; i < moves.length; i++) {
+        const [r, c] = moves[i];
+        const player = i % 2 === 0 ? 'A' : 'B';
+        grid[r][c] = player;
         if (i < 4) continue;
 
-        let winner = grid[1][1];
-        if (winner && ((winner === grid[0][0] && winner === grid[2][2])
-            || (winner === grid[2][0] && winner === grid[0][2])
-            || (winner === grid[1][0] && winner === grid[1][2])
-            || (winner === grid[0][1] && winner === grid[2][1]))) return winner;
-
-        winner = grid[0][0];
-        if (winner && (winner === grid[1][0] && winner === grid[2][0])
-        || (winner && (winner === grid[0][1] && winner === grid[0][2]))) return winner;
-
-        winner = grid[2][2];
-        if (winner && (winner === grid[2][1] && winner === grid[2][0])
-        || (winner && (winner === grid[0][2] && winner === grid[1][2]))) return winner;
+        if (grid[r][0] === player && grid[r][1] === player && grid[r][2] === player) return player;
+        if (grid[0][c] === player && grid[1][c] === player && grid[2][c] === player) return player;
+        if (r === c && grid[0][0] === player && grid[1][1] === player && grid[2][2] === player) return player;
+        if (r + c === 2 && grid[2][0] === player && grid[1][1] === player && grid[0][2] === player) return player;
     }
-    if (i < 9) return 'Pending';
-    return 'Draw';
+    return moves.length === 9 ? 'Draw' : 'Pending';
 };
