9c8c6969ec56f4e7608b59311ab479fe8c4493fa
Gayoung Kim <lotte1071103@naver.com>
2026-03-29 23:56:03 +0100

    Time: 0 ms (100.00%) | Memory: 56 MB (44.56%) - LeetSync

--- LeetCode/1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.js
+++ LeetCode/1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.js
@@ -6,9 +6,7 @@ var checkStraightLine = function(coordinates) {
     const [x1, y1] = coordinates[0];
     const [x2, y2] = coordinates[1];
     let g = (y2 - y1) / (x2 - x1);
-    let b;
-    if (g === Infinity) b = y1;
-    else b = y1 - g * x1;
+    const b = y1 - g * x1;
     for (let i = 2; i < coordinates.length; i++) {
         const [tx, ty] = coordinates[i];
         if (g === Infinity) {
