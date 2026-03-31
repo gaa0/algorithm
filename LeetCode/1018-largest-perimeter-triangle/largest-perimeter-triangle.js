36bdc683aad2398a5639c6c1258704bd12179800
Gayoung Kim <lotte1071103@naver.com>
2026-03-31 22:28:14 +0100

    Time: 27 ms (28.29%) | Memory: 58.8 MB (28.29%) - LeetSync

--- LeetCode/1018-largest-perimeter-triangle/largest-perimeter-triangle.js
+++ LeetCode/1018-largest-perimeter-triangle/largest-perimeter-triangle.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var largestPerimeter = function(nums) {
-    const sortedNums = nums.sort((a, b) => b - a);
-    for (let i = 0; i < sortedNums.length - 2; i++) {
-        if (sortedNums[i] < sortedNums[i + 1] + sortedNums[i + 2]) return sortedNums[i] + sortedNums[i + 1] + sortedNums[i + 2]
+    nums.sort((a, b) => b - a);
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] < nums[i + 1] + nums[i + 2]) return nums[i] + nums[i + 1] + nums[i + 2]
     }
     return 0;
 };
