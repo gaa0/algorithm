7bcc17a2c7969daac26307772dd959fb18bf3d5f
Gayoung Kim <lotte1071103@naver.com>
2026-04-04 20:27:32 +0100

    Time: 1 ms (50.00%) | Memory: 55.2 MB (7.41%) - LeetSync

--- LeetCode/1119-robot-bounded-in-circle/robot-bounded-in-circle.js
+++ LeetCode/1119-robot-bounded-in-circle/robot-bounded-in-circle.js
@@ -3,11 +3,10 @@
  * @return {boolean}
  */
 var isRobotBounded = function(instructions) {
-    const instructions4 = instructions + instructions + instructions + instructions;
     let [x, y, dx, dy] = [0, 0, 0, 1];
     const directions = [[0, 1], [-1, 0], [0, -1], [1, 0]];
     let d = 0;
-    for (const instruction of instructions4) {
+    for (const instruction of instructions) {
         if (instruction === "G") {
             [x, y] = [x + dx, y + dy];
         }
@@ -16,9 +15,8 @@ var isRobotBounded = function(instructions) {
         } else {
             d = (d + 3) % 4;
         }
-        console.log(d);
         [dx, dy] = directions[d];
     }
-    if (!x && !y) return true;
-    return false;
+    if (!d && !(!x && !y)) return false;
+    return true;
 };
