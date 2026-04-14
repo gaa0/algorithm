060d9ae7007b9e6fcb86031104214b1d63515468
Gayoung Kim <lotte1071103@naver.com>
2026-04-14 23:43:21 +0100

    Time: 11 ms (25.33%) | Memory: 62.5 MB (22.91%) - LeetSync

--- LeetCode/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
+++ LeetCode/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -3,14 +3,15 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
+    const vs = new Set(['a', 'e', 'i', 'o', 'u']);
     const vowels = [];
     for (const str of s) {
-        if (['a', 'e', 'i', 'o', 'u'].includes(str.toLowerCase())) vowels.push(str);
+        if (vs.has(str.toLowerCase())) vowels.push(str);
     }
     let ans = '';
     let i = vowels.length - 1;
     for (const str of s) {
-        if (['a', 'e', 'i', 'o', 'u'].includes(str.toLowerCase())) {
+        if (vs.has(str.toLowerCase())) {
             ans += vowels[i];
             i--;
         } else {
